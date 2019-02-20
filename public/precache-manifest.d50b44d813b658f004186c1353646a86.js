46d836f6262d126464f5665685b0ea474781c0c8
Juhi Trivedi <juhi.trivedi@multidots.com>
2019-02-20 18:40:20 +0530

    First commit

---
+++ public/precache-manifest.d50b44d813b658f004186c1353646a86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "392bc6b27bba8e553ae3",
+    "url": "/static/js/main.392bc6b2.chunk.js"
+  },
+  {
+    "revision": "158d54aae68555ff32fe",
+    "url": "/static/js/2.158d54aa.chunk.js"
+  },
+  {
+    "revision": "392bc6b27bba8e553ae3",
+    "url": "/static/css/main.005a5e3f.chunk.css"
+  },
+  {
+    "revision": "d2d036cc16f339ad4fbb19d152e090a1",
+    "url": "/index.html"
+  }
+];
